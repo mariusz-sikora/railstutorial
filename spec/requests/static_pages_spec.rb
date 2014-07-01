f9527956dfb8b50873584695ed96c23d5316b575
mariusz1109 <msikora87@gmail.com>
2014-07-01 22:12:33 +0200

    Added let for rspec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  let(:title) { "Ruby on Rails Tutorial Sample App | " }
+  
   describe "Home page" do
   
     it "should have the content 'Sample App'" do
@@ -10,7 +12,7 @@ describe "Static pages" do
     
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title(title.to_s + "Home")
     end
   end
   
@@ -23,7 +25,7 @@ describe "Static pages" do
     
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title(title.to_s + "Help")
     end
   end
   
@@ -36,7 +38,7 @@ describe "Static pages" do
     
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title(title.to_s + "About Us")
     end
   end
   
